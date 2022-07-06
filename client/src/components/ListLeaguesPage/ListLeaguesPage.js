192dc55043ff3d7ced21555dcf67aa7b286f93bc
APysnack <apysnack@gmail.com>
2022-07-06 09:15:35 -0400

    displays logos if applicable on the league list page

--- client/src/components/ListLeaguesPage/ListLeaguesPage.js
+++ client/src/components/ListLeaguesPage/ListLeaguesPage.js
@@ -2,18 +2,37 @@ import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_LEAGUES } from "./gql";
 import { Link } from "react-router-dom";
-import { LeagueListContainer } from "./ListLeagues.styles";
+import { LeagueListContainer, LeagueLink } from "./ListLeagues.styles";
+import { Avatar } from "@mui/material";
+const { REACT_APP_SERVER_URL } = process.env;
 
 function ListLeaguesPage() {
   const { loading, data } = useQuery(GET_LEAGUES);
+
+  React.useEffect(() => {
+    console.log(data);
+  }, [data]);
 
   if (loading) return "Loading...";
   return (
     <LeagueListContainer>
       {data.leagues.map((league) => (
-        <Link to={`/league/${league.id}`} key={league.id}>
-          {league.leagueName}
-        </Link>
+        <LeagueLink to={`/league/${league.id}`} key={league.id}>
+          {league.logoUrl ? (
+            <Avatar
+              src={REACT_APP_SERVER_URL + league.logoUrl}
+              sx={{ width: 100, height: 100 }}
+              className="leagueLogo"
+            />
+          ) : (
+            <Avatar
+              src={null}
+              sx={{ width: 100, height: 100 }}
+              className="leagueLogo"
+            />
+          )}
+          <div>{league.leagueName}</div>
+        </LeagueLink>
       ))}
     </LeagueListContainer>
   );
--- client/src/components/ListLeaguesPage/gql.js
+++ client/src/components/ListLeaguesPage/gql.js
@@ -5,6 +5,7 @@ export const GET_LEAGUES = gql`
     leagues {
       id
       leagueName
+      logoUrl
     }
   }
 `;

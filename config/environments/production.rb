ad6c879ddb90b9ba542f311b9eeb16344b47a2ef
APysnack <60242065+APysnack@users.noreply.github.com>
2023-10-20 22:30:52 -0400

    eh

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
-  config.secret_key_base = ENV.fetch('AWS_RDS_HOST')
+  config.secret_key_base = ENV.fetch('AWS_SECRET_KEY_BASE')
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"

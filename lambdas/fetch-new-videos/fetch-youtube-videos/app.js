3972c0f585f2386a8f9bd046caaf34d2701ec760
APysnack <60242065+APysnack@users.noreply.github.com>
2023-11-09 10:27:47 -0500

    split video fetcher into 2 lambdas wip

---
+++ lambdas/fetch-new-videos/fetch-youtube-videos/app.js
@@ -0,0 +1,62 @@
+const {
+  formatDate,
+  fetchVideosFromChannel,
+  createBattlesFor,
+} = require('./utils');
+const { connectToDatabase } = require('./pgFunctions');
+
+exports.lambdaHandler = async (event, context) => {
+  try {
+    const client = await connectToDatabase();
+
+    const queryResult = await client.query(
+      `SELECT * FROM leagues ORDER BY league_name ASC OFFSET $1 LIMIT $2;`,
+      [event.startPosition, event.recordsToFetch]
+    );
+
+    const leagues = queryResult.rows;
+
+    for (const league of leagues) {
+      let fetchAllVideos = false;
+
+      if (!league.videos_initialized) {
+        fetchAllVideos = true;
+      }
+
+      let nextPageToken = null;
+      let newVideos = null;
+      const processedUrls = [];
+
+      const channelId = league.league_url;
+
+      const response = await fetchVideosFromChannel(
+        channelId,
+        null,
+        formatDate(league.last_video_fetch_date)
+      );
+
+      nextPageToken = response.nextPageToken;
+      newVideos = response.videos;
+
+      if (newVideos.length > 0) {
+        createBattlesFor(newVideos, league, processedUrls);
+      }
+    }
+
+    const lambdaResponse = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: '',
+      }),
+    };
+    return lambdaResponse;
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: 500, // Internal Server Error
+      body: JSON.stringify({
+        message: 'An error occurred while processing your request.',
+      }),
+    };
+  }
+};

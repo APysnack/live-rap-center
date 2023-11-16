8c7498405de6a7983386a97d96a152e2eae6045f
APysnack <60242065+APysnack@users.noreply.github.com>
2023-11-16 13:26:25 -0500

    add bg job

--- app/sidekiq/fetch_new_videos_job.rb
+++ app/sidekiq/fetch_new_videos_job.rb
@@ -1,7 +1,11 @@
+require 'securerandom'
+
 class FetchNewVideosJob
   include Sidekiq::Job
 
   def perform(*args)
     puts "Fetching new videos... WORKING KINDAIDK"
+    random_url = SecureRandom.hex(8)
+    Battle.create(title: "placeholder", league_id: 1, battle_url: random_url, youtube_date: Time.now)
   end
 end
